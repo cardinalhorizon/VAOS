830e3712c2ba4bff784240cfd75f3b240a14a91f
BossOfGames <taylorbroad@gmail.com>
2017-01-21 00:26:57 -0500

    Removed phpVMS code. Not needed anymore.

--- app/Http/Controllers/Admin/InstallController.php
+++ app/Http/Controllers/Admin/InstallController.php
@@ -12,26 +12,35 @@ class InstallController extends Controller
 {
     public function index(Request $request)
     {
-        // Return the view right now
-        if ($request->query('fresh')
-            return view('install.fresh');
+        if(!Schema::hasTable('users')) {
+            // Return the view right now
+            if ($request->query('fresh'))
+                return view('install.fresh');
+            else
+                return view('install.start');
+        }
         else
-            return view('install.start');
+        {
+            return redirect('/');
+        }
     }
     public function doInstall(Request $request)
     {
-        // Run the database migration
-        $exitcode = Artisan::call('migrate');
-        DB::table('users')->insert([
-            'first_name' => $request->input('first_name'),
-            'last_name' => $request->input('last_name'),
-            'email' => $request->input('email'),
-            'username' => $request->input('username'),
-            'password' => bcrypt($request->input('password')),
-            'status' => 1,
-            'admin' => true
-        ]);
-        Auth::login(1);
-        return redirect('/admin');
+        if(!Schema::hasTable('users')) {
+            // Run the database migration
+            $exitcode = Artisan::call('migrate');
+            DB::table('users')->insert([
+                'first_name' => $request->input('first_name'),
+                'last_name' => $request->input('last_name'),
+                'email' => $request->input('email'),
+                'username' => $request->input('username'),
+                'password' => bcrypt($request->input('password')),
+                'status' => 1,
+                'admin' => true
+            ]);
+            Auth::login(1);
+            return redirect('/admin');
+        }
+        else return redirect('/');
     }
 }
