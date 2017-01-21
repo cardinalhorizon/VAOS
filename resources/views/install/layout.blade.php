0081edf095c1c739f6cbbf1a1fac6e66121380eb
BossOfGames <taylorbroad@gmail.com>
2017-01-21 01:18:53 -0500

    Fixed Installer copy screwup :/

--- resources/views/install/layout.blade.php
+++ resources/views/install/layout.blade.php
@@ -10,5 +10,20 @@
     <link href="{{URL::asset('crewops/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 </head>
 <body>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-sm-12">
+            <img src="img/VAOSLogo.png" style="width: 100%; margin-top: 20px;"/>
+        </div>
+        <div class="col-lg-8 col-sm-12">
+            <h1>Virtual Airline Operations System Web Setup</h1>
+            @yield('pageinfo')
+        </div>
+    </div>
     @yield('content')
+</div>
+    <nav class="navbar navbar-default navbar-fixed-bottom">
+        <p class="pull-left navbar-text">Virtual Airline Operations System Version 1.0.0 Beta 2</p>
+        <p class="pull-right navbar-text">Copyright 2017 Cardinal Horizon</p>
+    </nav>
 </body>
