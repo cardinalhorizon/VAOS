50e8546fe80be4a828b2a97d2253d244d8148de5
BossOfGames <taylorbroad@gmail.com>
2017-01-24 03:12:57 -0500

    Fixed DataTables Autowidth

--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -58,7 +58,8 @@
     <script type="text/javascript">
         $(document).ready( function () {
             $('#table_id').DataTable( {
-                responsive: true
+                responsive: true,
+                "autoWidth": false
             });
             $(".clickable-row").click(function() {
                 window.document.location = $(this).data("href");
