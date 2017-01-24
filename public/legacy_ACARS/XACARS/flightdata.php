a3a2f9f3706ecd3bf4f368f10c5087603f6329ad
BossOfGames <taylorbroad@gmail.com>
2017-01-24 18:04:03 -0500

    Fixed ACARS Bidding Bug

--- public/legacy_ACARS/XACARS/flightdata.php
+++ public/legacy_ACARS/XACARS/flightdata.php
@@ -17,11 +17,12 @@ function returnData($result)
     echo $result['aircraft']['icao']."\n";
 }
 // If Bid is placed, get the first for the user
-if (strtolower($_REQUEST['DATA2']) == "bid") {
+
     $res = $client->request('GET', VAOS_URL . 'api/v1/bids', [
         'query' => [
             'format' => 'xacars',
-            'username' => $_REQUEST['DATA4']
+            'username' => $_REQUEST['DATA4'],
+            'flightnum' => $_REQUEST['DATA2']
         ]
     ])->getBody();
     $jdec = json_decode($res, true);
@@ -29,23 +30,6 @@ if (strtolower($_REQUEST['DATA2']) == "bid") {
         echo "0|No Bids Found In System";
     else
     	returnData($jdec['bid']);
-}
-
-// If specific bid number entered, get that instead.
-else if (is_numeric($_REQUEST['DATA2']))
-{
-    $res = $client->request('GET', VAOS_URL . 'api/v1/bids', [
-        'query' => [
-            'format' => 'xacars',
-            'username' => $_REQUEST['DATA4'],
-			'bidid' => $_REQUEST['DATA2']
-        ]
-    ])->getBody();
-    $jdec = json_decode($res, true);
-}
-else
-    echo "0|Flightnumber not found";
-
 /*
 $query = mysql_query("SEECT * FROM `flights` where flightnumber='".$_REQUEST['DATA2']."'");
 $num_result = mysql_num_rows($query);
