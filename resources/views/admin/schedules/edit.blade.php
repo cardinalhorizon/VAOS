b2023797dcb58bec8893c6da8191283b5738f86f
Mark Swan <mark.swan@outlook.com.au>
2017-01-26 02:27:45 +0800

    Added editing function for schedule

--- resources/views/admin/schedules/edit.blade.php
+++ resources/views/admin/schedules/edit.blade.php
@@ -4,12 +4,14 @@
     <li class="breadcrumb-item">Home</li>
     <li class="breadcrumb-item"><a href="{{ url('admin/') }}">Admin</a></li>
     <li class="breadcrumb-item"><a href="{{ url('admin/schedule') }}">Schedule</a></li>
-    <li class="breadcrumb-item active">Edit Route</li>
+    <li class="breadcrumb-item active">Edit Route ({{ $schedule->airline->icao . $schedule->flightnum }})</li>
 @endsection
 
 @section('content')
-    <form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/schedule') }}">
+    <form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/schedule/'.$schedule->id) }}">
         {{csrf_field()}}
+        {{ method_field('PUT') }}
+
         <div class="col-lg-4">
             <div class="card">
                 <div class="card-header">
@@ -21,7 +23,7 @@
                         <div class="col-md-9">
                             <select id="airline" name="airline" class="form-control" size="1">
                                 @foreach($airlines as $a)
-                                    <option value="{{ $a->id }}">{{ $a->icao }} - {{ $a->name }}</option>
+                                    <option value="{{ $a->id }}" @if($a->id == $schedule->airline->id) selected="selected" @endif>{{ $a->icao }} - {{ $a->name }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -29,19 +31,29 @@
                     <div class="form-group row">
                         <label class="col-md-3 form-control-label" for="text-input">Flight Number</label>
                         <div class="col-md-9">
-                            <input type="text" id="flightnum" name="flightnum" class="form-control" placeholder="eg. 1126">
+                            <input type="text" id="flightnum" name="flightnum" class="form-control" placeholder="eg. 1126" value="{{ $schedule->flightnum }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 form-control-label" for="text-input">Departure</label>
                         <div class="col-md-9">
-                            <input type="text" id="depicao" name="depicao" class="form-control" placeholder="eg. KLAX">
+                            <input type="text" id="depicao" name="depicao" class="form-control" placeholder="eg. KLAX" value="{{ $schedule->depapt->icao }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 form-control-label" for="text-input">Arrival</label>
                         <div class="col-md-9">
-                            <input type="text" id="arricao" name="arricao" class="form-control" placeholder="eg. KSEA">
+                            <input type="text" id="arricao" name="arricao" class="form-control" placeholder="eg. KSEA" value="{{ $schedule->arrapt->icao }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 form-control-label"></label>
+                        <div class="col-md-9">
+                            <div class="checkbox">
+                                <label for="checkbox1">
+                                    <input type="checkbox" id="enabled" name="enabled" @if($schedule->enabled) checked="checked" @endif value="1"> Enabled
+                                </label>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -58,7 +70,7 @@
                         <div class="col-md-9">
                             <select id="aircraft_group" name="aircraft_group" class="form-control" size="1">
                                 @foreach($acfgroups as $acf)
-                                    <option value="{{ $acf->id }}">{{ $acf->name }}</option>
+                                    <option value="{{ $acf->id }}" @if($acf->id == $schedule->aircraft_group->id) selected="selected" @endif>{{ $acf->name }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -77,22 +89,12 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-3 form-control-label"></label>
-                        <div class="col-md-9">
-                            <div class="checkbox">
-                                <label for="checkbox1">
-                                    <input type="checkbox" id="enabled" name="enabled" value="1"> Enabled
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group row">
                         <label class="col-md-3 form-control-label" for="select">Type</label>
                         <div class="col-md-9">
                             <select id="type" name="type" class="form-control" size="1">
                                 <option value="0">Please select</option>
-                                <option value="1">Scheduled Passenger</option>
-                                <option value="2">Scheduled Cargo</option>
+                                <option value="1" @if($schedule->type == 1) selected="selected" @endif>Scheduled Passenger</option>
+                                <option value="2" @if($schedule->type == 2) selected="selected" @endif>Scheduled Cargo</option>
                             </select>
                         </div>
                     </div>
