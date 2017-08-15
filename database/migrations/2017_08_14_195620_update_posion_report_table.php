97463325a1f92a22e8855ca1324b6ed1806dca69
Mario Fehr <mario.fehr@gmx.at>
2017-08-15 14:51:07 +0200

    Added  flighttime to pirep and distremain, timeremaining to acarsdata table
    update the getAcarsData in the AcarsAPI

---
+++ database/migrations/2017_08_14_195620_update_posion_report_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePosionReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('acarsdata', function (Blueprint $table) {
+            $table->string('online', 10);
+            $table->string('timeremaining', 6);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
