393ee2ca34565246886a1e2928e3b472cffff553
Broad <tbroad@fgcu.edu>
2017-09-30 08:29:02 -0400

    Fixed smartCARS Functionality. Added Fuel Used, Flight Logs and ACARS client definitions to the pirep table.

---
+++ database/migrations/2017_08_30_213055_add_acars_client_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAcarsClientColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pireps', function (Blueprint $table) {
+            $table->string('acars_client');
+            $table->string('fuel_used')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
