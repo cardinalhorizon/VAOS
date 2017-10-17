13e1ed0bf2e0ac423652b2397ca4475261057d92
Mario Fehr <mario.fehr@gmx.at>
2017-10-17 21:04:28 +0200

    add/refacored Airport Model

---
+++ app/Models/Airport.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Airport extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = ['id', 'name', 'city', 'county', 'iata','icao', 'lat', 'lon'];
+
+    public function schedules(){
+        return $this->hasMany('App\Schedules');
+    }
+}
