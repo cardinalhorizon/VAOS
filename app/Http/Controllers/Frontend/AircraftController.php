2932187359514df728d4b543b3743ac704ce5dbf
Mario Fehr <mario.fehr@gmx.at>
2017-10-23 16:31:53 +0200

    add new controller

---
+++ app/Http/Controllers/Frontend/AircraftController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Models\Aircraft;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AircraftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Aircraft  $aircraft
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Aircraft $aircraft)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Aircraft  $aircraft
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Aircraft $aircraft)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Aircraft  $aircraft
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Aircraft $aircraft)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Aircraft  $aircraft
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Aircraft $aircraft)
+    {
+        //
+    }
+}
