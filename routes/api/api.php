dd2285ef34fc488a495f59a8dd643017045247bc
Taylor Broad <taylor@cardinalhorizon.com>
2017-10-29 01:15:54 -0400

    Created Info Route at /api/ to provide system version information as well as give additional information for FS Hub and the VAOS Mobile App when dynamic colors are supported.
    Signed-off-by: Taylor Broad <taylor@cardinalhorizon.com>

--- routes/api/api.php
+++ routes/api/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+
+    $settings = new \App\VASystem\DBSettings();
+    return response()->json([
+        'version' => config('vaos.version'),
+        'app_settings' => [
+            'community_title' => $settings->get('community_title'),
+            'colors' => [
+                'primary' => $settings->get('app_color_primary'),
+                'secondary' => $settings->get('app_color_secondary'),
+                'accent' => $settings->get('app_color_secondary'),
+                'btn' => $settings->get('app_color_btn'),
+                'info' => $settings->get('app_color_info'),
+                'warning' => $settings->get('app_color_warning'),
+                'error' => $settings->get('app_color_error')
+            ],
+            'icon_url' => $settings->get('icon_url'),
+            'logo_url' => $settings->get('logo_url'),
+        ]
+    ]);
+});
