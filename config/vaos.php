c175a47b0d5241c95a011904232f17e538e5d18d
Taylor Broad <taylor@cardinalhorizon.com>
2017-10-29 01:14:12 -0400

    Create VAOS Settings File for Static Settings

---
+++ config/vaos.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Virtual Airline Operations System Main Configuration File
+ *
+ * If you are reading this, you should know before modifying anything within this file,
+ * it would be a bad idea unless you know what you're doing.
+ *
+ * Upon updates, if we add more features which require static configuration overrides,
+ * you should know this file will be overwritten and your changes will be lost.
+ *
+ * Therefore it is advised that you instead modify the .env file located within the main
+ * project directory.
+ *
+ * Most settings however are stored within the database in the new settings table for ease of modification.
+ */
+
+return [
+
+    /*
+     * System Version
+     */
+    'version' => '1.1.0',
+
+
+];
