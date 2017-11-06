6502395e0fb5e5d96988c0d3672315b100b53f63
Mario Fehr <mario.fehr@gmx.at>
2017-11-06 15:19:06 +0000

    Apply fixes from StyleCI

--- app/Services/AirportService.php
+++ app/Services/AirportService.php
@@ -2,12 +2,10 @@
 
 namespace App\Services;
 
-
 use GuzzleHttp\Client;
 
 class AirportService extends BaseService
 {
-
     protected $airportRepo;
 
     public function __construct()
@@ -17,31 +15,31 @@ class AirportService extends BaseService
 
     /**
      * @param $icao
+     *
      * @throws \Prettus\Validator\Exceptions\ValidatorException
      */
     public function create($icao)
     {
         // lets request the airport identifier from the central database
         $client = new Client();
-        $res = $client->request('GET', 'http://fsvaos.net/api/data/airports', [
+        $res    = $client->request('GET', 'http://fsvaos.net/api/data/airports', [
             'query' => [
                 'icao' => $icao,
-            ]
+            ],
         ])->getBody();
 
         // Add the airport to the database
         $data = json_decode($res, true);
 
         $this->airportRepo->create([
-            'id' => $data['airport']['id'],
-            'name' => $data['airport']['name'],
-            'icao' => $data['airport']['gps_code'],
-            'iata' => $data['airport']['iata_code'],
-            'lat' => $data['airport']['latitude_deg'],
-            'lon' => $data['airport']['longitude_deg'],
-            'city' => $data['airport']['municipality'],
-            'country' => $data['airport']['iso_country']
+            'id'      => $data['airport']['id'],
+            'name'    => $data['airport']['name'],
+            'icao'    => $data['airport']['gps_code'],
+            'iata'    => $data['airport']['iata_code'],
+            'lat'     => $data['airport']['latitude_deg'],
+            'lon'     => $data['airport']['longitude_deg'],
+            'city'    => $data['airport']['municipality'],
+            'country' => $data['airport']['iso_country'],
         ]);
     }
-
 }
