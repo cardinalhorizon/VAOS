3f2c2e7fb0d110d00ea7f72f23fdd410ca6ee7d9
Mario Fehr <mario.fehr@gmx.at>
2017-11-09 15:25:56 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Frontend/AirlineController.php
+++ app/Http/Controllers/Frontend/AirlineController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Models\Airline;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreAirlineRequest;
 use App\Http\Requests\UpdateAirlineRequest;
