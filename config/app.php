e69b222f682f69a2dacf2d021e89f2570deceaff
Mario Fehr <mario.fehr@gmx.at>
2017-11-09 13:29:17 +0000

    Apply fixes from StyleCI

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-
         /*
          *  VAOS Service Providers...
          */
