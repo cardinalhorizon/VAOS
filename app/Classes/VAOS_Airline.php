2358190703a9a786824682c76a38d263bad67235
BossOfGames <taylorbroad@gmail.com>
2018-03-05 14:03:32 -0500

    Multi-Airline Boilerplate

---
+++ app/Classes/VAOS_Airline.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taylorbroad
+ * Date: 1/1/18
+ * Time: 7:20 PM
+ */
+
+namespace App\Classes;
+
+
+class VAOS_Airline
+{
+    public function newAirline($data)
+    {
+        //
+    }
+    public function addUserToAirline($airline_id, $user_id)
+    {
+        //
+    }
+}
