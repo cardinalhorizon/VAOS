f5f34564fc4f4fedc660d93d2c8c44dd7cb9eabb
BossOfGames <taylorbroad@gmail.com>
2018-03-05 14:17:35 -0500

    News System Database

---
+++ database/migrations/2018_01_02_083826_create_news_system.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsSystem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $t){
+            $t->increments('id');
+            $t->string('title');
+            $t->string('url_slug');
+            $t->unsignedInteger('airline_id');
+            $t->unsignedInteger('user_id');
+            $t->text('content');
+            $t->string('cover_url');
+            $t->timestamps();
+            $t->boolean('published');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
