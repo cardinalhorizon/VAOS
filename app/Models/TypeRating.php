37be12ac05b880d8268fa8559b1dfd413dd13251
BossOfGames <taylorbroad@gmail.com>
2018-03-05 14:15:01 -0500

    Type Rating Relationships

---
+++ app/Models/TypeRating.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TypeRating extends Model
+{
+    public function user()
+    {
+        return $this->belongsToMany('App\User');
+    }
+    public function airline()
+    {
+        return $this->belongsTo('App\Models\Airline');
+    }
+    public function aircraft()
+    {
+        return $this->belongsToMany('App\Models\Aircraft');
+    }
+    public function aircraft_groups()
+    {
+        return $this->belongsToMany('App\Models\AircraftGroup');
+    }
+}
