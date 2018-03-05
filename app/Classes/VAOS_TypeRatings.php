782ae2c501a7db930553b334dd0fd25e810f4d46
BossOfGames <taylorbroad@gmail.com>
2018-03-05 14:02:10 -0500

    Created Type Ratings Class File

---
+++ app/Classes/VAOS_TypeRatings.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taylorbroad
+ * Date: 3/5/18
+ * Time: 1:57 PM
+ */
+
+namespace App\Classes;
+
+
+class VAOS_TypeRatings
+{
+    public static function AddTypeRating()
+    {
+        //
+    }
+    public static function ModifyTypeRating($rating_id, $data)
+    {
+        //
+    }
+    public static function AddUserToTypeRating($rating_id, $user_id)
+    {
+        //
+    }
+    public static Function VerifyRating($rating_id, $user_id)
+    {
+        //
+    }
+}
