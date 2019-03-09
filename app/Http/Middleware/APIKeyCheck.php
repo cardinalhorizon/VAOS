19f4756af551071f5f99b0cdc52d2fb150eb9665
BossOfGames <taylorbroad@gmail.com>
2019-03-09 17:23:06 -0500

    API key chaeck for later

---
+++ app/Http/Middleware/APIKeyCheck.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+
+class APIKeyCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $dbKey = DB::table('api_keys')->where('key', $request->query('key'))->first();
+        if(!empty($dbKey))
+            return $next($request);
+        else
+            return response('Invalid Key', 403);
+    }
+}
