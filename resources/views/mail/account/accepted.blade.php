f1a241761b82d49a0b24363c034c90389f76b620
BossOfGames <taylorbroad@gmail.com>
2019-03-09 17:09:59 -0500

    Modified accepted email script

--- resources/views/mail/account/accepted.blade.php
+++ resources/views/mail/account/accepted.blade.php
@@ -1,15 +1,15 @@
 @component('mail::message')
 
-Hi {{ $user->first_name }},
+    Hi {{ $user->first_name }},
 
-We are please to announce your account has been accepted and we would like to welcome you to {{ config('app.name') }}.
+    We are please to announce your account has been accepted and we would like to welcome you to {{ config('app.name') }}.
 
-To get started, all you need to do is click the button below to take you to the login screen and subsequently the dashboard.
+    To get started, all you need to do is click the button below to take you to the login screen and subsequently the dashboard.
 
-@component('mail::button', ['url' => url('/login')])
-Login Now
-@endcomponent
+    @component('mail::button', ['url' => url('/login')])
+        Login Now
+    @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }} Staff
+    Thanks,<br>
+    {{ config('app.name') }} Staff
 @endcomponent
