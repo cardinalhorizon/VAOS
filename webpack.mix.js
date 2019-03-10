709b4688628397225ff950ed7afad9dcaac0dd64
BossOfGames <taylorbroad@gmail.com>
2019-03-10 15:53:21 -0400

    Fixed Laravel Mix Issue

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +11,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/admin/admin.scss', 'public/css');
+mix
+  .js("resources/assets/js/app.js", "public/js")
+  .sass("resources/assets/sass/app.scss", "public/css")
+  .sass("resources/assets/sass/admin/admin.scss", "public/css")
+  .version();
