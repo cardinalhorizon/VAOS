0cfcc13b5365ef0255b2c13e7749ed1d0dcaf820
BossOfGames <taylorbroad@gmail.com>
2019-07-12 21:54:29 -0400

    Add Home Airport ID to User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedInteger('home_airport_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
