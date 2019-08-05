4b0c156c68ef8d8c41403532956a5a20f6ca9170
Taylor Broad <taylor@cardinalhorizon.com>
2019-08-04 22:20:47 -0400

    Combine Multiple Flights Into a Trip

---
+++ database/migrations/2019_08_03_031151_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('aviation_group_id')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('flight_trip', function(Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('flight_id');
+            $table->unsignedBigInteger('trip_id');
+            $table->integer('order');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
