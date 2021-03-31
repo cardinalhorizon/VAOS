e4504a8006eec08249693702fdcd9c286369e4f8
Taylor Broad <taylorbroad@gmail.com>
2021-03-31 00:08:37 -0700

    Modified default user table to VAOS standards.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('status');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('vatsim')->nullable();
+            $table->string('ivao')->nullable();
+            $table->string('pilot_edge')->nullable();
+            $table->string('poscon')->nullable();
+            $table->unsignedBigInteger('location_id');
+            $table->string('avatar_url')->nullable();
+            $table->string('cover_url')->nullable();
             $table->timestamps();
         });
     }
