19d6db2b1ed929e166377a3e300ce17e5cf0437d
Taylor Broad <taylorbroad@gmail.com>
2022-01-23 23:23:42 -0700

    Base AircraftController.php functions

---
+++ app/Http/Controllers/API/AircraftController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AircraftController extends Controller
+{
+    //
+    public function index() {
+        //
+    }
+    public function getAircraft($id) {}
+    public function modifyAircraft($id, Request $request) {}
+    public function createAircraft(Request $request) {}
+}
