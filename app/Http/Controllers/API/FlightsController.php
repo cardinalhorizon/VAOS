9c7e4ded40a0f3f69404c87abc33b6aee8e43fdb
Taylor Broad <taylorbroad@gmail.com>
2022-01-23 23:24:10 -0700

    Base FlightsController.php functions

--- app/Http/Controllers/API/FlightsController.php
+++ app/Http/Controllers/API/FlightsController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class FlightsController extends Controller
 {
-    //
+    public function index() {
+
+    }
+    public function getFlight($id) {}
+    public function getFlights(Request $request) {}
+    public function searchFlights(Request $request) {}
+    public function createFlight(Request $request) {
+        //
+    }
+    public function modifyFlight(Request $request) {}
+    public function positionReport(Request $request) {}
+    public function startFlight($id) {
+        //
+    }
+    public function endFlight($id) {}
 }
