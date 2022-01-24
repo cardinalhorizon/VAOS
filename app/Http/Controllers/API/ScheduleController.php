23285d913bc83829d9e6df650f259df4c1fa3b9f
Taylor Broad <taylorbroad@gmail.com>
2022-01-23 23:23:52 -0700

    Base ScheduleController.php functions

---
+++ app/Http/Controllers/API/ScheduleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ScheduleController extends Controller
+{
+    public function getRoutes(Request $request) {}
+    public function getRoute($id) {}
+    public function createRoute(Request $request) {}
+    public function modifyRoute($id, Request $request) {}
+    public function deleteRoute($id) {}
+    public function createBid($id, Request $request) {}
+}
